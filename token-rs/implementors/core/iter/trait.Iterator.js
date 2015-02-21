af7016dc448a0cedb8afc3b6b047c884f0df439b
Jakob Lautrup Nysom <jaln@itu.dk>
2015-02-21 17:01:04 +0100

    Token-rs documentation

---
+++ token-rs/implementors/core/iter/trait.Iterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['token'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;'a, R: <a class='trait' href='http://doc.rust-lang.org/nightly/std/io/trait.Read.html' title='std::io::Read'>Read</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='token/struct.Tokenizer.html' title='token::Tokenizer'>Tokenizer</a>&lt;R&gt;","<a class='stability Stable' title='Stable'></a>impl&lt;'a, 'b, R: <a class='trait' href='http://doc.rust-lang.org/nightly/std/io/trait.Read.html' title='std::io::Read'>Read</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='token/struct.SentenceSplitter.html' title='token::SentenceSplitter'>SentenceSplitter</a>&lt;'a, R&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
